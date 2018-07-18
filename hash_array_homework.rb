27c67a900e952bb9e8f989ca36954f5995556e3b
Shannon Holmes <snholmes13@gmail.com>
2018-07-18 20:56:41 +0100

    get Avril's even lottery numbers

--- hash_array_homework.rb
+++ hash_array_homework.rb
@@ -135,6 +135,9 @@ p users["Avril"][:pets][0][:species]
 p users["Erik"][:lottery_numbers].min
 
 # 6. Return an array of Avril's lottery numbers that are even
+
+p users["Avril"][:lottery_numbers].select { |num| num.even? }
+
 # 7. Erik is one lottery number short! Add the number 7 to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "Fluffy"
