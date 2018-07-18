9981241eb79301a9f3f3154116c329f669ce46d3
Shannon Holmes <snholmes13@gmail.com>
2018-07-18 17:01:21 +0100

    add Polmont

--- hash_array_homework.rb
+++ hash_array_homework.rb
@@ -16,6 +16,9 @@ p stops << "Edinburgh Waverley"
 p stops.unshift "Glasgow Queen St"
 
 # 3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+
+p stops.insert(4, "Polmont")
+
 # 4. Work out the index position of "Linlithgow"
 # 5. Remove "Livingston" from the array using its name
 # 6. Delete "Cumbernauld" from the array by index
