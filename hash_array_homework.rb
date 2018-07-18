8d3b886ea9ac5d735ab087c602f67674dcb3f8d5
Shannon Holmes <snholmes13@gmail.com>
2018-07-18 21:10:38 +0100

    change Erik's hometown to Edinburgh

--- hash_array_homework.rb
+++ hash_array_homework.rb
@@ -143,6 +143,10 @@ p users["Avril"][:lottery_numbers].select { |num| num.even? }
 p users["Erik"][:lottery_numbers] << 7
 
 # 8. Change Erik's hometown to Edinburgh
+
+users["Erik"][:home_town] = "Edinburgh"
+p users["Erik"][:home_town]
+
 # 9. Add a pet dog to Erik called "Fluffy"
 # 10.  Add another person to the users hash
 # ============================================
