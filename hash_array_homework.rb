a96fd12f9f3db7dcabe7df14163c1121cbe8c765
Shannon Holmes <snholmes13@gmail.com>
2018-07-18 23:25:56 +0100

    add comment out work, so does not print everything at once

--- hash_array_homework.rb
+++ hash_array_homework.rb
@@ -159,57 +159,57 @@
 # p users.merge(new_users)
 
 # ============================================
-# Exercise C
+# # Exercise C
+# #
+# # Given the following data structure:
+# #
+# united_kingdom = [
+#   {name: "Scotland", population: 5295000, capital: "Edinburgh"},
+#   {
+#     name: "Wales",
+#     population: 3063000,
+#     capital: "Swansea"
+#   },
+#   {
+#     name: "England",
+#     population: 53010000,
+#     capital: "London"
+#   }
+# ]
+# # Complete these tasks:
+# #
+# # # 1. Change the capital of Wales from "Swansea" to "Cardiff".
+# #
+# # united_kingdom[1][:capital]= "Cardiff"
+# # p united_kingdom
+# #
+# # # 2. Create a Hash for Northern Ireland and add it to the united_kingdom array (The capital is Belfast, and the population is 1,811,000).
+# #
+# # united_kingdom << {name: "Northern Ireland", population: 1811000, capital: "Belfast"}
+# # p united_kingdom
+# #
+# # # 3. Use a loop to print the names of all the countries in the UK.
+# #
+# # for countries in united_kingdom
+# #   p countries[:name]
+# # end
 #
-# Given the following data structure:
+# # 4. Use a loop to find the total population of the UK.
 #
-united_kingdom = [
-  {name: "Scotland", population: 5295000, capital: "Edinburgh"},
-  {
-    name: "Wales",
-    population: 3063000,
-    capital: "Swansea"
-  },
-  {
-    name: "England",
-    population: 53010000,
-    capital: "London"
-  }
-]
-# Complete these tasks:
-#
-# # 1. Change the capital of Wales from "Swansea" to "Cardiff".
-#
-# united_kingdom[1][:capital]= "Cardiff"
-# p united_kingdom
-#
-# # 2. Create a Hash for Northern Ireland and add it to the united_kingdom array (The capital is Belfast, and the population is 1,811,000).
-#
-# united_kingdom << {name: "Northern Ireland", population: 1811000, capital: "Belfast"}
-# p united_kingdom
-#
-# # 3. Use a loop to print the names of all the countries in the UK.
-#
-# for countries in united_kingdom
-#   p countries[:name]
-# end
-
-# 4. Use a loop to find the total population of the UK.
-
-  population_total = 0
-    for country in united_kingdom
-      population_total += country[:population]
-    end
-p population_total.to_s() + " is the total population of the UK."
-
-# DID NOT WORK - BUT WHY???
-# # def total_population(united_kingdom)
 #   population_total = 0
 #     for country in united_kingdom
 #       population_total += country[:population]
-#       # country[:population] = 0
 #     end
-#     # return population_total.to_s() + "is the total UK population."
-# # end
+# p population_total.to_s() + " is the total population of the UK."
 #
-# p population_total
+# # DID NOT WORK - BUT WHY???
+# # # def total_population(united_kingdom)
+# #   population_total = 0
+# #     for country in united_kingdom
+# #       population_total += country[:population]
+# #       # country[:population] = 0
+# #     end
+# #     # return population_total.to_s() + "is the total UK population."
+# # # end
+# #
+# # p population_total
