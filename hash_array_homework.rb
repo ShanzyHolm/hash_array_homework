a485d72961b63f79d7aefe8c302575d47dd934f6
Shannon Holmes <snholmes13@gmail.com>
2018-07-18 21:01:46 +0100

    add 7 to Erik's lottery numbers

--- hash_array_homework.rb
+++ hash_array_homework.rb
@@ -139,6 +139,9 @@ p users["Erik"][:lottery_numbers].min
 p users["Avril"][:lottery_numbers].select { |num| num.even? }
 
 # 7. Erik is one lottery number short! Add the number 7 to be included in his lottery numbers
+
+p users["Erik"][:lottery_numbers] << 7
+
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "Fluffy"
 # 10.  Add another person to the users hash
